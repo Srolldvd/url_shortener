d97f8d99b81be0acedf6c2c5a91982905a9b0fc2
srolldvd <106958353+Srolldvd@users.noreply.github.com>
2023-10-10 16:17:28 +0000

    Update 20 files

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,11 @@
 import "./styles.scss";
 
+/* 
+$base-font-size: 18px;
+$heading-scale: 8; // amount of px headings grow from h6 to h1
 
+@for $i from 1 through 6 {
+  h#{$i} {
+    font-size: $base-font-size + $heading-scale * (6 - $i);
+  }
+} */
