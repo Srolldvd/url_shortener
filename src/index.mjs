00ceef92af818e6c9afae9b68f22c41c0723f7c9
srolldvd <106958353+Srolldvd@users.noreply.github.com>
2023-10-13 17:34:00 +0000

    Styling header + hero section

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,1 @@
 import "./styles.scss";
-
-/* 
-$base-font-size: 18px;
-$heading-scale: 8; // amount of px headings grow from h6 to h1
-
-@for $i from 1 through 6 {
-  h#{$i} {
-    font-size: $base-font-size + $heading-scale * (6 - $i);
-  }
-} */
